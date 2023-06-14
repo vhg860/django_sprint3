66a4d9dfaad05bee91e5cfc349a1123e5ac96fe5
Дмитрий Истомин <vhg860@gmail.com>
2023-06-14 17:26:49 +0300

    4

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-
 from django.db import models
 
 User = get_user_model()
@@ -7,8 +6,10 @@ User = get_user_model()
 
 class BaseModel(models.Model):
     is_published = models.BooleanField(
-        default=True, verbose_name='Опубликовано',
-        help_text='Снимите галочку, чтобы скрыть публикацию.')
+        'Опубликовано',
+        default=True,
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
+    )
     created_at = models.DateTimeField(auto_now_add=True,
                                       verbose_name='Добавлено'
                                       )
@@ -18,64 +19,74 @@ class BaseModel(models.Model):
 
 
 class Category(BaseModel):
-    title = models.CharField(max_length=256, verbose_name='Заголовок')
-    description = models.TextField(verbose_name='Описание')
+    title = models.CharField(
+        'Заголовок',
+        max_length=256
+    )
+    description = models.TextField('Описание')
     slug = models.SlugField(
-        verbose_name='Идентификатор',
+        'Идентификатор',
         help_text='Идентификатор страницы для URL; '
-        'разрешены символы латиницы, цифры, дефис и подчёркивание.',
-        unique=True)
+                  'разрешены символы латиницы, цифры, дефис и подчёркивание.',
+        unique=True
+    )
 
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'Категории'
 
     def __str__(self):
-        return self.title
+        return f'{self.title[:20]}'
 
 
 class Location(BaseModel):
-    name = models.CharField(max_length=256, verbose_name='Название места')
+    name = models.CharField(
+        'Название места',
+        max_length=256
+    )
 
     class Meta:
         verbose_name = 'местоположение'
         verbose_name_plural = 'Местоположения'
 
     def __str__(self):
-        return self.name
+        return f'{self.name[:20]}'
 
 
 class Post(BaseModel):
-    title = models.CharField(max_length=256, verbose_name='Заголовок')
-    text = models.TextField(verbose_name='Текст')
+    title = models.CharField(
+        'Заголовок',
+        max_length=256
+    )
+    text = models.TextField('Текст')
     pub_date = models.DateTimeField(
-        verbose_name='Дата и время публикации',
+        'Дата и время публикации',
         help_text='Если установить дату и время в будущем — можно делать'
-        ' отложенные публикации.')
+        ' отложенные публикации.'
+        )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, verbose_name='Автор публикации')
+        User,
+        on_delete=models.CASCADE,
+        related_name='+',
+        verbose_name='Автор публикации',
+    )
     location = models.ForeignKey(
         Location,
         on_delete=models.SET_NULL,
-        related_name='posts',
-        null=True, verbose_name='Местоположение')
+        blank=True, verbose_name='Местоположение'
+        )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
-        related_name='posts',
         null=True,
-        verbose_name='Категория')
-    is_published = models.BooleanField(
-        default=True, verbose_name='Опубликовано',
-        help_text='Снимите галочку, чтобы скрыть публикацию.')
-    created_at = models.DateTimeField(auto_now_add=True,
-                                      verbose_name='Добавлено'
-                                      )
+        verbose_name='Категория'
+        )
 
     class Meta:
         verbose_name = 'публикация'
         verbose_name_plural = 'Публикации'
         ordering = ('-pub_date',)
+        default_related_name = 'posts'
 
     def __str__(self):
-        return self.title
+        return f'{self.title[:20]}'
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,6 +1,5 @@
-import datetime
-
 from django.shortcuts import get_object_or_404, render
+from django.utils import timezone
 
 from blog.models import Category, Post
 
@@ -8,26 +7,30 @@ from blog.models import Category, Post
 def output_published(posts_object):
     return (
         posts_object.filter(
-            is_published=True, category__is_published=True
-        )
-        .filter(pub_date__date__lte=datetime.datetime.today()))
+            is_published=True, category__is_published=True,
+            pub_date__date__lte=timezone.now()
+            ))
 
 
 def index(request):
     return render(request, 'blog/index.html', {
-        'post_list': output_published(Post.objects.all(),)[:5], })
+        'post_list': output_published(Post.objects.all(),)[:5],
+        })
 
 
 def post_detail(request, post_id):
-    return render(request, 'blog/detail.html', {
-        'post': get_object_or_404(
-            output_published(Post.objects.all(),), pk=post_id,),
-    })
+    return render(
+        request, 'blog/detail.html',
+        {'post': get_object_or_404(
+            output_published(Post.objects.all(),), pk=post_id,)},
+        )
 
 
 def category_posts(request, category_slug):
-    category = get_object_or_404(Category.objects.all(), slug=category_slug,
-                                 is_published=True)
+    category = get_object_or_404(Category,
+                                 slug=category_slug,
+                                 is_published=True
+                                 )
     return render(request, 'blog/category.html', {
         'category': category,
         'post_list': output_published(category.posts,)},)
